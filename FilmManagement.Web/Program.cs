baa1d59a0ca11cabd0904d4575be33fac5094f32
Thiago Fontoura <thiago.fontoura120@gmail.com>
2025-07-05 02:56:14 -0300

    Adição do MudBlazor

--- FilmManagement.Web/Program.cs
+++ FilmManagement.Web/Program.cs
@@ -1,10 +1,13 @@
 using FilmManagement.Web;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddMudServices();
 
 builder.Services.AddTransient<FilmmakerAPI>();
 
