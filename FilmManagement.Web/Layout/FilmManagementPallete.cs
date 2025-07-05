2296a5c39630203887d8fb7e1b4c66bf7d5af9f1
Thiago Fontoura <thiago.fontoura120@gmail.com>
2025-07-05 03:23:47 -0300

    Personalizando Ambiente

---
+++ FilmManagement.Web/Layout/FilmManagementPallete.cs
@@ -0,0 +1,18 @@
+﻿using MudBlazor;
+using MudBlazor.Utilities;
+
+namespace FilmManagement.Web.Layout
+{
+    public sealed class FilmManagementPallete : PaletteDark
+    {
+
+        private FilmManagementPallete()
+        {
+            Primary = new MudColor("#9966FF");
+            Secondary = new MudColor("#F6AD31");
+            Tertiary = new MudColor("#8AE491");
+        }
+
+        public static FilmManagementPallete CreatePallete => new();
+    }
+}
