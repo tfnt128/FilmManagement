755c7624657bcaf6a4716832371bb44576897670
Thiago Fontoura <thiago.fontoura120@gmail.com>
2025-07-16 05:50:39 -0300

    Exclusão de Filmmaker através da Aplicação(não pode excluir se tiver um filme relacionado ao filmmaker especifico)

--- FilmManagement.Web/Services/FilmmakerAPI.cs
+++ FilmManagement.Web/Services/FilmmakerAPI.cs
@@ -14,11 +14,21 @@ public class FilmmakerAPI
     public async Task<ICollection<FilmmakerResponse>?> GetFilmmakersAsync()
     {
         return await
-        _httpClient.GetFromJsonAsync<ICollection<FilmmakerResponse>>("Filmmakers");
+        _httpClient.GetFromJsonAsync<ICollection<FilmmakerResponse>>("filmmakers");
     }
 
     public async Task AddFilmmakerAsync(FilmmakerRequest filmmaker)
     {
-        await _httpClient.PostAsJsonAsync("Filmmakers", filmmaker);
+        await _httpClient.PostAsJsonAsync("filmmakers", filmmaker);
+    }
+
+    public async Task DeleteFilmmakerAsync(int id)
+    {
+        await _httpClient.DeleteAsync($"filmmakers/{id}");
+    }
+
+    public async Task<FilmmakerResponse?> GetFilmmakerNameAsync(string name)
+    {
+        return await _httpClient.GetFromJsonAsync<FilmmakerResponse>($"filmmakers/{name}");
     }
 }
